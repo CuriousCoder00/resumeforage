b12e628ddab0c4cdd8564e70b28aaa0803fce135
Kapil Jangid <kapiljangid040@gmail.com>
2024-12-15 12:25:45 +0530

    refactor: simplify database client initialization

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    // eslint-disable-line no-var
-    var prisma: PrismaClient | undefined;
-}
-
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+const prisma = new PrismaClient();
+export const db = prisma;
